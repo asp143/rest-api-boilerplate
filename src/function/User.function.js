4c5dd7e2e353f25edf3441b85a46c74fd6d22af1
asp143 <mungcs09@gmail.com>
2020-06-04 21:04:35 +0800

    Added new use cases

---
+++ src/function/User.function.js
@@ -0,0 +1,101 @@
+/**
+ * This is where we put all the use cases for the user model
+ */
+const UserDb = require('../db/helper/User.helper');
+const Response = require('../class/Response');
+const logger = require('../../logger/logger').log('User Functions');
+
+// Utils
+const { decrypt } = require('../utils/bcrypt');
+const { CreateToken } = require('../utils/jwt');
+
+module.exports = {
+
+    Register: async (newUser) => {
+        const response = new Response();
+        try {
+            logger.info('Creating a user..');
+            const user = await UserDb.Create(newUser);
+
+            response.setPayload(user);
+            return response;
+        } catch (error) {
+            response.setFailed(500, error);
+            return response;
+        }
+    },
+    Login: async (email, password) => {
+        const response = new Response();
+        logger.info(`logging in user with ${email}:${password}`);
+        try {
+            const userFound = await UserDb.FindOne({ email });
+
+            if (userFound) {
+                // check if password is correct
+                if (!decrypt(password, userFound.password)) {
+                    response.setFailed(401, `Invalid password for ${email}`);
+                } else {
+                    const token = await CreateToken(userFound);
+                    response.setPayload({ token });
+                }
+            } else {
+                response.setFailed(404, `Account not found: ${email}`);
+            }
+
+            return response;
+        } catch (error) {
+            response.setFailed(500, error);
+            return response;
+        }
+    },
+    FindOne: async (query) => {
+        const response = new Response();
+        try {
+            const userData = await UserDb.FindOne(query);
+
+            if (!userData) {
+                response.setFailed(404, 'No user found..');
+                return response;
+            }
+
+            response.setPayload(userData);
+            return response;
+        } catch (error) {
+            response.setFailed(500, error);
+            return response;
+        }
+    },
+    FindOneById: async (id) => {
+        const response = new Response();
+        try {
+            const userData = await UserDb.FindOneById(id);
+            if (!userData) {
+                response.setFailed(404, 'No user found..');
+                return response;
+            }
+
+            response.setPayload(userData);
+            return response;
+        } catch (error) {
+            response.setFailed(500, error);
+            return response;
+        }
+    },
+    FindAll: async (query) => {
+        const response = new Response();
+        try {
+            const userData = await UserDb.FindAll(query);
+
+            if (!userData) {
+                response.setFailed(202, 'No user found..');
+                return response;
+            }
+
+            response.setPayload(userData);
+            return response;
+        } catch (error) {
+            response.setFailed(500, error);
+            return response;
+        }
+    },
+};
